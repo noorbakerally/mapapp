81a2d4207e1ad515285b3e93f40aa3cd4cc91cce
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-09-20 16:27:01 +0200

    map added

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,9 @@
-angular.module('myApp').controller('controllername', function($scope) {
+angular.module('myApp').controller('mapController', function($scope) {
+	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoiYmFrZW5vb3IiLCJhIjoiY2l0YjNjczhpMDAxMzJ1cDc5aXVxeHZ5NiJ9.8YzFSaP-znz8JUeHYybuTg', {
 
+			maxZoom: 18,
+			id: 'bakenoor.1eh9o2c2'
+
+		}).addTo(mymap);
 });
