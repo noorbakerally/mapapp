ef334fa1587fc65fbad21a589fbb641322fdcb59
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-09-20 16:41:08 +0200

    geo center of map set to grenoble

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,7 @@
 angular.module('myApp').controller('mapController', function($scope) {
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	var mymap = L.map('mapid').setView([45.1, 5.7], 8);
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoiYmFrZW5vb3IiLCJhIjoiY2l0YjNjczhpMDAxMzJ1cDc5aXVxeHZ5NiJ9.8YzFSaP-znz8JUeHYybuTg', {
-
 			maxZoom: 18,
 			id: 'bakenoor.1eh9o2c2'
-
 		}).addTo(mymap);
 });
