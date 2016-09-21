5341de126ae9453a0f6d0e24c6bb67442c4a3e01
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-09-21 20:29:23 +0200

    zoom level changed

--- js/controllers.js
+++ js/controllers.js
@@ -83,7 +83,7 @@ angular.module('myApp').controller('OneGroupViewerController', function($scope,$
 });
 
 angular.module('myApp').controller('mapController', function($scope,$rootScope) {
-	var mymap = L.map('mapid').setView([45.1, 5.7], 8);
+	var mymap = L.map('mapid').setView([45.1, 5.7], 3);
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoiYmFrZW5vb3IiLCJhIjoiY2l0YjNjczhpMDAxMzJ1cDc5aXVxeHZ5NiJ9.8YzFSaP-znz8JUeHYybuTg', {
 			maxZoom: 18,
 			id: 'bakenoor.1eh9o2c2'
